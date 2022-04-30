7ff6a2d7ac05379b212314a963a98bf052a1d539
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-04-29 21:04:46 -0300

    created singleton to use one instace database

--- app/src/main/java/br/com/borges/lucas/mydiary/service/repository/MemoryRepository.kt
+++ app/src/main/java/br/com/borges/lucas/mydiary/service/repository/MemoryRepository.kt
@@ -1,8 +1,22 @@
 package br.com.borges.lucas.mydiary.service.repository
 
+import android.content.Context
 import br.com.borges.lucas.mydiary.service.model.MemoryModel
 
-class MemoryRepository {
+class MemoryRepository private constructor( context: Context ) {
+
+  private var mMemoryDataBaseHelper: MemoryDataBaseHelper = MemoryDataBaseHelper( context )
+
+  companion object {
+    private lateinit var repository: MemoryRepository
+
+    fun getInstance( context: Context ) : MemoryRepository {
+      if ( !::repository.isInitialized ) {
+        repository = MemoryRepository( context )
+      }
+      return repository
+    }
+  }
 
   fun getAll(): List<MemoryModel> {
     val list: MutableList<MemoryModel> = ArrayList()
--- app/src/main/java/br/com/borges/lucas/mydiary/viewmodel/MemoryViewModel.kt
+++ app/src/main/java/br/com/borges/lucas/mydiary/viewmodel/MemoryViewModel.kt
@@ -1,15 +1,16 @@
 package br.com.borges.lucas.mydiary.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import br.com.borges.lucas.mydiary.service.repository.MemoryRepository
 import br.com.borges.lucas.mydiary.service.model.MemoryModel
-import java.util.*
+import br.com.borges.lucas.mydiary.service.repository.MemoryRepository
 
-class MemoryViewModel : ViewModel() {
+class MemoryViewModel( application: Application ) : AndroidViewModel( application ) {
 
-  private val mMemoryRepository: MemoryRepository = MemoryRepository()
+  private val mContext = application.applicationContext
+  private val mMemoryRepository: MemoryRepository = MemoryRepository.getInstance( mContext )
 
   private var mSaveMemory = MutableLiveData<Boolean>()
   val saveMemory: LiveData<Boolean> = mSaveMemory
