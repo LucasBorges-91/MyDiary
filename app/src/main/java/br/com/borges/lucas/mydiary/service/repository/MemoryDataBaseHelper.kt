f5f9badc92aa3f338ff9e1d3dac6f6d1121d7312
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-06-27 21:39:16 -0300

    adding layer database

--- app/src/main/java/br/com/borges/lucas/mydiary/service/repository/MemoryDataBaseHelper.kt
+++ app/src/main/java/br/com/borges/lucas/mydiary/service/repository/MemoryDataBaseHelper.kt
@@ -3,9 +3,15 @@ package br.com.borges.lucas.mydiary.service.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import br.com.borges.lucas.mydiary.service.constants.DataBaseConstants
+import br.com.borges.lucas.mydiary.service.model.MemoryModel
 
-class MemoryDataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+/*class MemoryDataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
   override fun onCreate(db: SQLiteDatabase) {
     db.execSQL(CREATE_TABLE_MEMORY)
   }
@@ -25,4 +31,32 @@ class MemoryDataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABAS
           + DataBaseConstants.MEMORY.COLUMNS.MEMORY + " text, "
           + DataBaseConstants.MEMORY.COLUMNS.DATE + " text);")
   }
+}*/
+@Database( entities = [MemoryModel::class], version = 1 )
+abstract class MemoryDataBase() : RoomDatabase() {
+
+  companion object {
+    private lateinit var INSTANCE: MemoryDataBase
+
+    fun getDataBase( context: Context ) : MemoryDataBase {
+      synchronized(MemoryDataBase::class) {
+        if ( !::INSTANCE.isInitialized ) {
+          return Room.databaseBuilder( context, MemoryDataBase::class.java, "memorydb")
+            .addMigrations()
+            .allowMainThreadQueries()
+            .build()
+        }
+      }
+      return INSTANCE
+    }
+
+    //corresponde ao onUnpgrade
+    private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+      override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DELETE FROM Memory")
+      }
+
+    }
+  }
+
 }
